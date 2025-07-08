f53113f5b890f84045ffc53c8134f5fa4a4cff71
raiyashpratap@gmail.com <raiyashpratap@gmail.com>
2025-07-08 08:27:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import WeatherPage from './pages/WeatherPage';
+
 function App() {
   return (
-    <div className="App">
-      <WeatherPage/>
-    </div>
+    <>
+      {/* Weather background elements */}
+      <div className="sun"></div>
+      <div className="cloud cloud1"></div>
+      <div className="cloud cloud2"></div>
+      <div className="cloud cloud3"></div>
+      {/* Main app container */}
+      <div className="App">
+        <WeatherPage />
+      </div>
+    </>
   );
 }
 
